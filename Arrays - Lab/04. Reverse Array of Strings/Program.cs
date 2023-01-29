f56fd873d1a38565e93028b712e616cfa3ad2510
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-01-29 22:08:13 +0200

    Other Array Exercises

---
+++ Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fundamentals_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{words[i]} ");
+            }
+        }
+    }
+}
---
+++ Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int evenSum = 0;
+int oddSum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    {
+        evenSum += numbers[i];
+    }
+    else
+    {
+        oddSum += numbers[i];
+    }
+}
+Console.WriteLine(evenSum - oddSum);
