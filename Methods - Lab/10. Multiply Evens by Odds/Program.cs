546a0d51453df1a17da0552084e21a6c97770fdf
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-03 22:40:49 +0200

    More exercises

---
+++ Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsString = Console.ReadLine();
+            int number = int.Parse(numberAsString);
+            int result = GetMultipleOfEvenAndOdds(number);
+            Console.WriteLine(result);
+        }
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int evenSum = GetSumOfEvenDigits(number);
+            int oddSum = GetSumOfOddDigits(number);
+            int sum = evenSum * oddSum;
+            return sum;
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            int absoluteNumber = Math.Abs(number);
+
+            while (absoluteNumber > 0)
+            {
+                int lastDigit = absoluteNumber % 10;
+
+                if (lastDigit % 2 == 0)
+                {
+                    sum += lastDigit; 
+                }
+
+                absoluteNumber = absoluteNumber / 10;
+            }
+            return sum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            int absoluteNumber = Math.Abs(number);
+
+            while (absoluteNumber > 0)
+            {
+                int lastDigit = absoluteNumber % 10;
+
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+
+                absoluteNumber = absoluteNumber / 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Methods - Lab/11. Math operations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            string givenOperator = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double sum = OperationBetweenNumbers(firstNumber, givenOperator, secondNumber);
+            Console.WriteLine(sum);
+
+        }
+
+        static double OperationBetweenNumbers(double firstNumber, string givenOperator, double secondNumber)
+        {
+            double sum = 0;
+            if (givenOperator == "+")
+            {
+                sum = firstNumber + secondNumber;
+            }
+            else if (givenOperator == "-")
+            {
+                sum = firstNumber - secondNumber;
+            }
+            else if (givenOperator == "*")
+            {
+                sum = firstNumber * secondNumber;
+            }
+            else if (givenOperator == "/")
+            {
+                sum = firstNumber / secondNumber;
+            }
+
+            return sum;
+        }
+    }
+}
