6761aa5f47a325f4b3c4f0b07bd3650ab8e657ad
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-03 22:40:15 +0200

    Method exercises

---
+++ Methods - Lab/07. Repeat String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = RepeatString(word, n);
+            Console.WriteLine(result);
+        }
+
+        static string RepeatString(string word, int n)
+        {
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                result+= word;
+            }
+            return result;
+        }
+    }
+}
---
+++ Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = PowerNumber(number, power);
+            Console.WriteLine(result);
+        }
+
+        private static double PowerNumber(double number, int power)
+        {
+            double result = Math.Pow(number, power);
+            return result;
+        }
+    }
+}
