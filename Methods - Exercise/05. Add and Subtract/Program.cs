f6bc8edefe484b07f265b6a2e71060d3bf872481
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-05 23:11:57 +0200

    Some method exercises

---
+++ Methods - Exercise/05. Add and Subtract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int addResult = AddNumbers(firstNum, secondNum);
+            int subtractionResult = SubtractNumbers(addResult, thirdNum);
+            Console.WriteLine(subtractionResult);
+        }
+
+        static int AddNumbers(int firstNum, int secondNum)
+        { 
+            int result = firstNum + secondNum;
+            return result;
+        }
+
+        static int SubtractNumbers(int addResult, int thirdNumber) 
+        {
+            int result = addResult - thirdNumber;
+            return result;
+        }
+    }
+}
---
+++ Methods - Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            GetWordMiddleChars(word);
+        }
+        static void GetWordMiddleChars(string word)
+        {
+            if (word.Length % 2 == 0)
+            {
+                Console.WriteLine($"{word[(word.Length / 2) - 1]}{word[word.Length / 2]}");
+            }
+            else
+            {
+                Console.WriteLine(word[(word.Length / 2)]);
+            }
+        }
+    }
+}
