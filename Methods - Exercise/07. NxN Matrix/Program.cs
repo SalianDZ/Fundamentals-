80c066425b33599a2fcb0a0604a3e81fe6cbad08
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-05 23:12:23 +0200

    More exercises

---
+++ Methods - Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);
+        }
+        static void PrintMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
---
+++ Methods - Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            double firstNumberFactorial = GetFactorial(firstNumber);
+            int secondNumber = int.Parse(Console.ReadLine());
+            double secondNumberFactorial = GetFactorial(secondNumber);
+            double result = firstNumberFactorial / secondNumberFactorial;
+            Console.WriteLine($"{result:f2}");
+        }
+        static int GetFactorial(int number)
+        {
+            int fact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}
