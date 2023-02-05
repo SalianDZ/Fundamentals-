05eeab06f4e9f2ffca183f47a59bb6f7af5c0011
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-05 23:12:50 +0200

    Additional method exercises

---
+++ Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            CheckNumber(input);
+        }
+        static void CheckNumber(string input)
+        {
+            int reversedNumber = 0;
+            string reversedNumberAsString = "";
+            string numberAsString = "";
+            while (input != "END")
+            {
+                int number = int.Parse(input);
+                if (number < 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    numberAsString = number.ToString();
+                    char[] numberAsStringInChars = numberAsString.ToCharArray();
+                    Array.Reverse(numberAsStringInChars);
+                    reversedNumberAsString = new string(numberAsStringInChars);
+                    reversedNumber = int.Parse(reversedNumberAsString);
+                }
+
+                if (number == reversedNumber)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Methods - Exercise/10. Top Number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace _10._Top_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumOfDigits = 0;
+            bool isDivisibleByEight = false;
+            bool hasOddDigit = false;
+
+            for (int i = 8; i <= n; i++)
+            {
+                int iCopy = i;
+                string iAsString = i.ToString();
+                for (int j = 0; j < iAsString.Length; j++)
+                {
+                    int currentDigit = iCopy % 10;
+                    if (currentDigit % 2 != 0)
+                    {
+                        hasOddDigit = true;
+                    }
+                    sumOfDigits = sumOfDigits + currentDigit;
+                    iCopy = iCopy / 10;
+                }
+
+                if (sumOfDigits % 8 == 0)
+                {
+                    isDivisibleByEight = true;
+                }
+                sumOfDigits = 0;
+
+                if (isDivisibleByEight == true && hasOddDigit == true)
+                {
+                    Console.WriteLine(i);
+                }
+                hasOddDigit = false;
+                isDivisibleByEight = false;
+            }
+        }
+    }
+}
