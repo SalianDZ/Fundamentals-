c053c0da69043b6a2cae63b2826ffcb02f7e853f
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-08 22:54:13 +0200

    My first List exercises

---
+++ Lists - Lab/02. Gauss' Trick/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+List<int> result = new List<int>();
+
+for (int i = 0; i < numbers.Count / 2; i++)
+{
+    int currentSum = numbers[i] + numbers[numbers.Count - 1 - i];
+    result.Add(currentSum);
+}
+if (numbers.Count % 2 != 0)
+{
+    Console.WriteLine(string.Join(" ", result) + " " + numbers[numbers.Count / 2]);
+}
+else
+{
+    Console.WriteLine(string.Join(" ", result));
+}
---
+++ Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,27 @@
+﻿List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+int minValue = Math.Min(firstList.Count, secondList.Count);
+
+List<int> result = new List<int>();
+for (int i = 0; i < minValue; i++)
+{
+    result.Add(firstList[i]);
+    result.Add(secondList[i]);
+}
+
+if (firstList.Count > secondList.Count)
+{
+    for (int i = minValue; i < firstList.Count; i++)
+    {
+        result.Add(firstList[i]);
+    }
+}
+else if (firstList.Count < secondList.Count)
+{
+    for (int i = minValue; i < secondList.Count; i++)
+    {
+        result.Add(secondList[i]);
+    }
+}
+Console.WriteLine(String.Join(" ", result));
