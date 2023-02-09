23dd5ea9757c0ad9ebddfe2c4bbffe6c9ca10372
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-09 22:31:31 +0200

    List exercises

---
+++ Lists - Lab/04. List of Products/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<string> products = new List<string>();
+
+for (int i = 0; i < n; i++)
+{
+    products.Add(Console.ReadLine());
+}
+products.Sort();
+
+for (int i = 0; i < products.Count; i++)
+{
+    Console.WriteLine($"{i + 1}.{products[i]}");
+}
---
+++ Lists - Lab/05. Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,21 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+	if (numbers[i] < 0)
+	{
+		numbers.RemoveAt(i);
+		i--;
+	}
+}
+if (numbers.Count <= 0)
+{
+	Console.WriteLine("empty");
+}
+else
+{
+    numbers.Reverse();
+    Console.WriteLine(String.Join(" ", numbers));
+}
+
+
