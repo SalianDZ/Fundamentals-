31dce418ff2e43f292d2da4122b2b27770fbda9c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-02-22 23:04:04 +0200

    Objects and Classes - first exercises

---
+++ Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] words = Console.ReadLine().Split();
+Random random = new Random();
+
+for (int i = 0; i < words.Length; i++)
+{
+    int randomIndex = random.Next(0, words.Length);
+    string currentWord = words[i];
+    string randomisedWord = words[randomIndex];
+
+    words[i] = randomisedWord;
+    words[randomIndex] = currentWord;
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, words));
---
+++ Objects and Classes - Lab/03.Songs/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+int numberOfSongs = int.Parse(Console.ReadLine());
+List<Song> songList = new List<Song>();
+
+for (int i = 0; i < numberOfSongs; i++)
+{
+    string[] info = Console.ReadLine().Split("_"); 
+
+    string typeList = info[0];
+    string songName = info[1];
+    string songDuration = info[2];
+
+    Song song = new Song();
+
+    song.TypeList = typeList;
+    song.Name = songName;
+    song.Duration = songDuration;
+
+    songList.Add(song);
+}
+
+string searchedTypeList = Console.ReadLine();
+
+if (searchedTypeList == "all")
+{
+    foreach (var song in songList)
+    {
+        Console.WriteLine(song.Name);
+    }
+}
+else
+{
+    foreach (var song in songList)
+    {
+        if (song.TypeList == searchedTypeList)
+        {
+            Console.WriteLine(song.Name);
+        }
+    }
+}
+
+class Song
+{
+    public string TypeList { get; set; }
+    public string Name { get; set; }
+    public string Duration { get; set; }
+}using System.Collections.Generic;
+using System.Linq;
+
+int numberOfSongs = int.Parse(Console.ReadLine());
+List<Song> songList = new List<Song>();
+
+for (int i = 0; i < numberOfSongs; i++)
+{
+    string[] info = Console.ReadLine().Split("_"); 
+
+    string typeList = info[0];
+    string songName = info[1];
+    string songDuration = info[2];
+
+    Song song = new Song();
+
+    song.TypeList = typeList;
+    song.Name = songName;
+    song.Duration = songDuration;
+
+    songList.Add(song);
+}
+
+string searchedTypeList = Console.ReadLine();
+
+if (searchedTypeList == "all")
+{
+    foreach (var song in songList)
+    {
+        Console.WriteLine(song.Name);
+    }
+}
+else
+{
+    foreach (var song in songList)
+    {
+        if (song.TypeList == searchedTypeList)
+        {
+            Console.WriteLine(song.Name);
+        }
+    }
+}
+
+class Song
+{
+    public string TypeList { get; set; }
+    public string Name { get; set; }
+    public string Duration { get; set; }
+}
