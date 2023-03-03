457d722f5e712900d1f9e37da8254a8a748ad979
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-03 23:37:33 +0200

    07. Company Users

---
+++ Associative Arrays - Exercise/07. Company Users/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection.Metadata;
+
+bool isContained = false;
+Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+while (true)
+{
+    string[] input = Console.ReadLine()
+        .Split(" -> ", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    if (input[0] == "End")
+    {
+        break;
+    }
+    string companyName = input[0];
+    string employeeId = input[1];
+
+    if (!companies.ContainsKey(companyName))
+    {
+        companies[companyName] = new List<string>();
+    }
+
+    //foreach (var (key, value) in companies.Where(x => x.Key == companyName))
+    //{
+    //    if (value.ToString() == employeeId)
+    //    {
+    //        isContained = true;
+    //        break;
+    //    }
+    //}
+
+    if (!isContained)
+    {
+        companies[companyName].Add(employeeId);
+    }
+}
+
+foreach (var item in companies)
+{
+    Console.WriteLine(item.Key);
+    foreach (var member in item.Value.Distinct())
+    {
+        Console.WriteLine($"-- {member}");
+    }
+}
+
+
