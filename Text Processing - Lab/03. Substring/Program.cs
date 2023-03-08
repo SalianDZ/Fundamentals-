60be613dfce9eaead81326181327d60a11a73541
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-08 22:54:02 +0200

    Text Processing - Lab

---
+++ Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,10 @@
+﻿string wordToRemove = Console.ReadLine();
+string word = Console.ReadLine();
+
+while (word.Contains(wordToRemove))
+{
+    int startIndex = word.IndexOf(wordToRemove);
+    word = word.Remove(startIndex, wordToRemove.Length);
+}
+
+Console.WriteLine(word);
