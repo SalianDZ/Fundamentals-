f2e8322e7be5d033fd9a0b655ebacb90f42c2313
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-08 23:12:04 +0200

    Text Processing - Lab

---
+++ Text Processing - Lab/04. Text Filter/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] bannedWords = Console.ReadLine().Split(", ");
+string text = Console.ReadLine();
+
+for (int i = 0; i < bannedWords.Length; i++)
+{
+	text = text.Replace(bannedWords[i], new string('*', bannedWords[i].Length));
+}
+
+Console.WriteLine(text);
---
+++ Text Processing - Lab/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,25 @@
+﻿string word = Console.ReadLine();
+string digits = string.Empty;
+string letters = string.Empty;
+string symbols = string.Empty;
+
+for (int i = 0; i < word.Length; i++)
+{
+	if (word[i] >= '0' && word[i] <= '9')
+	{
+		digits += word[i];
+	}
+	else if ((word[i] >= 'a' && word[i] <= 'z')
+		|| (word[i] >= 'A' && word[i] <= 'Z'))
+	{
+		letters += word[i];
+	}
+	else
+	{
+		symbols += word[i];
+	}
+}
+
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(symbols);
