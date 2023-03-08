4b0076ff2840f9b88597fc63f3e3548a8b3def78
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-08 22:42:46 +0200

    Text Processing - Lab

---
+++ Text Processing - Lab/01. Reverse Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+while (true)
+{
+    string currentWord = Console.ReadLine();
+	List<char> reversedWord = new List<char>();
+
+	if (currentWord == "end")
+	{
+		break;
+	}
+
+	for (int i = 0; i < currentWord.Length; i++)
+	{
+		reversedWord.Insert(0 , currentWord[i]);
+	}
+
+	Console.WriteLine($"{currentWord} = {string.Join("", reversedWord)}");
+}
