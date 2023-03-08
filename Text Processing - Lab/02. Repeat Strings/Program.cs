cc1b73c5809bd9ea11ede38a205ae7b81c028fd7
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-08 22:47:24 +0200

    Text Processing - Lab

---
+++ Text Processing - Lab/02. Repeat Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] words = Console.ReadLine().Split();
+string result = string.Empty;
+
+foreach (var word in words)
+{
+    int wordLenght = word.Length;
+	for (int i = 0; i < wordLenght; i++)
+	{
+		result += word;
+	}
+}
+Console.WriteLine(result);
