12b4c10a2ada416ebe974b002da58dbc103d0c81
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-10 21:44:48 +0200

    Text Processing - Exercise

---
+++ Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -0,0 +1,23 @@
+﻿List<string> usernames = Console.ReadLine().Split(", ").ToList();
+bool isValid = true;
+
+foreach (var username in usernames)
+{
+	if (username.Length >= 3 && username.Length <= 16)
+	{
+        foreach (char currentChar in username)
+        {
+            if (!(char.IsLetter(currentChar) || char.IsDigit(currentChar)
+                || (currentChar == '-') || (currentChar == '_')))
+            {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid)
+        {
+            Console.WriteLine(username);
+        }
+    }
+    isValid = true;
+}
---
+++ Text Processing - Exercise/02. Character Multiplier/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] words = Console.ReadLine().Split();
+string stringOne = words[0];
+string stringTwo = words[1];
+CharacterMultiplier(stringOne, stringTwo);
+
+
+
+
+void CharacterMultiplier(string stringOne, string stringTwo)
+{
+	int sum = 0;
+    int minLenght = Math.Min(stringOne.Length, stringTwo.Length);
+	int maxLength = Math.Max(stringOne.Length, stringTwo.Length);
+
+	for (int i = 0; i < minLenght; i++)
+	{
+		sum += stringOne[i] * stringTwo[i]; 
+	}
+
+
+	if (maxLength == stringOne.Length)
+	{
+        for (int i = minLenght; i < maxLength; i++)
+        {
+			sum += stringOne[i];
+        }
+    }
+	else if (maxLength == stringTwo.Length)
+	{
+        for (int i = minLenght; i < maxLength; i++)
+        {
+			sum += stringTwo[i];
+        }
+    }
+
+	Console.WriteLine(sum);
+}
