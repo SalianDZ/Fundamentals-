a35e78b56850964b67fdb75c5ce393228c92abc0
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-10 21:57:26 +0200

    04. Caesar Cipher

---
+++ Text Processing - Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,11 @@
+﻿string text = Console.ReadLine();
+List<char> chars = new List<char>();
+
+for (int i = 0; i < text.Length; i++)
+{
+    int currentCharInInt = text[i] + 3;
+    char currentChar = (char) currentCharInInt;
+    chars.Add(currentChar);
+}
+
+Console.WriteLine(String.Join("", chars));
