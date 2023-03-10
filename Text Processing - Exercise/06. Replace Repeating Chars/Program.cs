80e7b7068f751a9d6b3cae5351e11c06bee9d597
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-10 22:20:45 +0200

    06. Replace Repeating Chars

---
+++ Text Processing - Exercise/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+string word = Console.ReadLine();
+StringBuilder sb = new StringBuilder();
+
+for (int i = 1; i < word.Length; i++)
+{
+    char previousChar = word[i - 1];
+
+	if (previousChar == word[i])
+	{
+		continue;
+	}
+	else
+	{
+		sb.Append(previousChar);
+	}
+}
+sb.Append(word[word.Length - 1]);
+Console.WriteLine(sb);
