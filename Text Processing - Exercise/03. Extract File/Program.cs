4e1dc040b3ec0da98f979f354962506bed2d9298
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-10 21:50:27 +0200

    03. Extract File

---
+++ Text Processing - Exercise/03. Extract File/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+string[] path = Console.ReadLine().Split("\\");
+string[] nameAndExtension = path[path.Length - 1].Split('.');
+string name = nameAndExtension[0];
+string extension = nameAndExtension[1];
+
+StringBuilder sb = new StringBuilder();
+sb.AppendLine($"File name: {name}");
+sb.AppendLine($"File extension: {extension}");
+
+Console.WriteLine(sb);
