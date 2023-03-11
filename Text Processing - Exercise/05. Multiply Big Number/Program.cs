955efebecfe2289b8e155fdaa7b812564c29098b
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-11 22:25:22 +0200

    Text Processing - Exercises

---
+++ Text Processing - Exercise/05. Multiply Big Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+string input = Console.ReadLine();
+int multiplier = int.Parse(Console.ReadLine());
+
+StringBuilder sb = new StringBuilder();
+int reminder = 0;
+
+if (multiplier == 0 || input == "0")
+{
+    Console.WriteLine(0);
+    return;
+}
+
+for (int i = input.Length - 1; i >= 0; i--)
+{
+    int currDigit = int.Parse(input[i].ToString());
+    int product = currDigit * multiplier + reminder;
+    int result = product % 10;
+    reminder = product / 10;
+    sb.Insert(0, result);
+}
+
+if (reminder > 0)
+{
+    sb.Insert(0, reminder);
+}
+Console.WriteLine(sb.ToString());
---
+++ Text Processing - Exercise/07. String Explosion/Program.cs
@@ -0,0 +1,53 @@
+﻿string word = Console.ReadLine();
+//char[] wordAsCharArray = word.ToCharArray();
+//List<char> wordAsCharList = word.Split().Select(char.Parse).ToList();
+List<char> wordAsCharList = new List<char>();
+wordAsCharList.AddRange(word);
+int power = 0;
+
+for (int i = 0; i < wordAsCharList.Count; i++)
+{
+	if (wordAsCharList[i] == '>')
+	{
+        if (wordAsCharList[i + 1] == '>')
+        {
+            continue;
+        }
+		char currentPowerAsChar = wordAsCharList[i + 1];
+        int currentPower = Convert.ToInt32(new string(currentPowerAsChar, 1));
+		power += currentPower;
+    }
+	else
+	{
+		if (char.IsDigit(wordAsCharList[i]))
+		{
+            if (power == 1)
+            {
+                wordAsCharList.RemoveAt(i);
+                i--;
+                power --;
+            }
+            else
+            {
+                power -= 1;
+                wordAsCharList.RemoveAt(i);
+                i--;
+            }
+        }
+        else
+        {
+            if (power <= 0)
+            {
+                continue;
+            }
+            else
+            {
+                wordAsCharList.RemoveAt(i);
+                i--;
+                power--;
+            }
+        }
+	}
+}
+
+Console.WriteLine(String.Join("", wordAsCharList));
