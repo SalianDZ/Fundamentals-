3f25d64906d7b16d8c40acf77f9ec30c5b7de420
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-15 21:47:08 +0200

    Regular Expressions - Lab

---
+++ Regular Expressions - Lab/03. Match Dates/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+string input = Console.ReadLine();
+string pattern = @"(?<day>\d{2})(\.|-|/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+MatchCollection matchCollection = Regex.Matches(input, pattern);
+
+foreach (Match match in matchCollection)
+{
+    var groups = match.Groups;
+
+    Console.WriteLine($"Day: {groups["day"]}, Month: {groups["month"]}, Year: {groups["year"]}");
+}
