b293e1ef89e320faac92614c16df86b1e51cc570
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-15 21:46:56 +0200

    Regular Expressions - Lab

---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+string pattern = @"\+359(-| )2\1\d{3}\1\d{4}\b";
+
+Regex Regex = new Regex(pattern);
+MatchCollection matches = Regex.Matches(text, pattern);
+
+Console.WriteLine(string.Join(", ", matches));
