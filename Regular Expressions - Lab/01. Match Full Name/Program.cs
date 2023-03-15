b0a4088bb7b08ee83300b8ab9c61d4105fdb2130
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-15 21:46:34 +0200

    Regular Expressions - Lab

---
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+Regex regex = new Regex(pattern);
+MatchCollection matches = regex.Matches(text);
+
+Console.WriteLine(string.Join(" ", matches));
