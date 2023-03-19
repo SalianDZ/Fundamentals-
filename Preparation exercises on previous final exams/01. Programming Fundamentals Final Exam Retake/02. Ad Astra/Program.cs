1e1ed6b2a2deee6a48c10ae004005e5aa298b3af
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-20 00:56:34 +0200

    Ad Astra
    It has to be fixed

---
+++ Preparation exercises on previous final exams/01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+
+string pattern = @"(\#|\|)(?<itemName>(([a-zA-Z]+)(\s)?([A-Za-z]+)?))(\1)(?<date>\d{2}\/\d{2}\/\d{2})(\1)(?<calories>\d+)(\1)";
+string text = Console.ReadLine();
+int sum = 0;
+
+Regex Regex = new Regex(pattern);
+MatchCollection matchCollection = Regex.Matches(text);
+Dictionary<string, FruitsInfo> foods = new Dictionary<string, FruitsInfo>();
+
+foreach (Match match in matchCollection)
+{
+    if (match.Success)
+    {
+        string currentDate = match.Groups["date"].Value;
+        int currentCalories = int.Parse(match.Groups["calories"].Value);
+        sum += currentCalories;
+        foods[match.Groups["itemName"].Value] = new FruitsInfo(currentDate, currentCalories);
+    }
+}
+
+
+Console.WriteLine($"You have food to last you for: {(int) (sum / (2000 * 1.0))} days!");
+foreach (var food in foods)
+{
+    Console.WriteLine($"Item: {food.Key}, Best before: {food.Value.ExpirationDate}, Nutrition: {food.Value.Calories}");
+}
+
+
+
+public class FruitsInfo 
+{
+    public FruitsInfo(string expirationDate, int calories)
+    {
+        ExpirationDate = expirationDate;
+        Calories = calories;
+    }
+
+    public string ExpirationDate { get; set; }
+
+    public int Calories { get; set; }
+}
