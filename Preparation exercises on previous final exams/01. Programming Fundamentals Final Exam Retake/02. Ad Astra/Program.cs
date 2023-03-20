98a786632dd613e333107f9874b10fbdb5b48ac8
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-20 23:04:54 +0200

    Fix of the exercise

--- Preparation exercises on previous final exams/01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
+++ Preparation exercises on previous final exams/01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
@@ -1,43 +1,21 @@
 ﻿using System.Reflection.Metadata;
 using System.Text.RegularExpressions;
 
-string pattern = @"(\#|\|)(?<itemName>(([a-zA-Z]+)(\s)?([A-Za-z]+)?))(\1)(?<date>\d{2}\/\d{2}\/\d{2})(\1)(?<calories>\d+)(\1)";
 string text = Console.ReadLine();
+string pattern = @"(\||\#)(?<food>[A-Za-z\s]+)\1(?<date>(\d{2}\/)(\d{2}\/)(\d{2}))\1(?<calories>\d+)\1";
 int sum = 0;
 
-Regex Regex = new Regex(pattern);
-MatchCollection matchCollection = Regex.Matches(text);
-Dictionary<string, FruitsInfo> foods = new Dictionary<string, FruitsInfo>();
+MatchCollection matchCollection = Regex.Matches(text, pattern);
 
 foreach (Match match in matchCollection)
 {
-    if (match.Success)
-    {
-        string currentDate = match.Groups["date"].Value;
-        int currentCalories = int.Parse(match.Groups["calories"].Value);
-        sum += currentCalories;
-        foods[match.Groups["itemName"].Value] = new FruitsInfo(currentDate, currentCalories);
-    }
+    int currentCalories = int.Parse(match.Groups["calories"].Value);
+    sum += currentCalories;
 }
 
 
-Console.WriteLine($"You have food to last you for: {(int) (sum / (2000 * 1.0))} days!");
-foreach (var food in foods)
+Console.WriteLine($"You have food to last you for: {sum / 2000} days!");
+foreach (Match match in matchCollection)
 {
-    Console.WriteLine($"Item: {food.Key}, Best before: {food.Value.ExpirationDate}, Nutrition: {food.Value.Calories}");
-}
-
-
-
-public class FruitsInfo 
-{
-    public FruitsInfo(string expirationDate, int calories)
-    {
-        ExpirationDate = expirationDate;
-        Calories = calories;
-    }
-
-    public string ExpirationDate { get; set; }
-
-    public int Calories { get; set; }
+    Console.WriteLine($"Item: {match.Groups["food"]}, Best before: {match.Groups["date"]}, Nutrition: {match.Groups["calories"]}");
 }
