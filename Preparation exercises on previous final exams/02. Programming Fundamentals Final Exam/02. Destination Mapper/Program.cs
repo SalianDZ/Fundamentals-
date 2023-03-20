fb6c3b455ba65c6cbf2d99cb9a5a276a77d80021
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-20 23:05:18 +0200

    Destination Mapper

---
+++ Preparation exercises on previous final exams/02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+string text = Console.ReadLine();
+string pattern = @"(\=|\/)(?<place>[A-Z][A-Za-z]{2,})\1";
+MatchCollection places = Regex.Matches(text, pattern);
+int sum = 0;
+List<string> placesList = new List<string>();
+
+foreach (Match place in places)
+{
+    sum += place.Groups["place"].Value.Length;
+    placesList.Add(place.Groups["place"].Value);
+}
+
+Console.WriteLine($"Destinations: {string.Join(", ", placesList)}");
+Console.WriteLine($"Travel Points: {sum}");
