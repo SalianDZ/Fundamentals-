8d32b4db569de8fda5d956081d880b3264c8f004
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-22 20:22:07 +0200

    02. Fancy Barcodes

---
+++ Preparation exercises on previous final exams/04. Programming Fundamentals Final Exam/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"(\@)#+(?<name>[A-Z][A-Za-z\d]{4,}[A-Z])\1#+";
+int n = int.Parse(Console.ReadLine());
+bool hasDigits = false;
+
+for (int i = 0; i < n; i++)
+{
+    string currentText = Console.ReadLine();
+    Match currentMatch = Regex.Match(currentText, pattern);
+
+    if (currentMatch.Success)
+    {
+        string group = string.Empty;
+        string currentMatchAsString = currentMatch.Value;
+
+        for (int j = 0; j < currentMatch.Length; j++)
+        {
+            if (char.IsDigit(currentMatchAsString[j]))
+            {
+                hasDigits = true;
+                group += currentMatchAsString[j];
+            }
+        }
+
+        if (hasDigits)
+        {
+            Console.WriteLine($"Product group: {group}");
+        }
+        else
+        {
+            Console.WriteLine("Product group: 00");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid barcode");
+    }
+    hasDigits = false;
+}
