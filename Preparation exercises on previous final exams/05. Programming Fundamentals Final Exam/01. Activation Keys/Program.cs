447062046fcb776ec6f3cd36b39fe2eb19684afc
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-25 18:36:28 +0200

    05. Programming Fundamentals Final Exam

---
+++ Preparation exercises on previous final exams/05. Programming Fundamentals Final Exam/01. Activation Keys/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+string activationKey = Console.ReadLine();
+
+while (true)
+{
+    string[] command = Console.ReadLine()
+        .Split(">>>").ToArray();
+
+    if (command[0] == "Generate")
+    {
+        break;
+    }
+    else if (command[0] == "Contains")
+    {
+        string substring = command[1];
+        if (activationKey.Contains(substring))
+        {
+            Console.WriteLine($"{activationKey} contains {substring}");
+        }
+        else
+        {
+            Console.WriteLine("Substring not found!");
+        }
+    }
+    else if (command[0] == "Flip")
+    {
+        string part = command[1];
+        int startIndex = int.Parse(command[2]);
+        int endIndex = int.Parse(command[3]);
+        string substring = activationKey.Substring(startIndex, (endIndex - startIndex));
+        if (part == "Upper")
+        {
+            string upperSubstring = substring.ToUpper();
+            activationKey = activationKey.Replace(substring, upperSubstring);
+        }
+        else if (part == "Lower")
+        {
+            string lowerSubstring = substring.ToLower();
+            activationKey = activationKey.Replace(substring, lowerSubstring);
+        }
+
+        Console.WriteLine(activationKey);
+    }
+    else if (command[0] == "Slice")
+    {
+        int startIndex = int.Parse(command[1]);
+        int endIndex = int.Parse(command[2]);
+
+        activationKey = activationKey.Remove(startIndex, (endIndex - startIndex));
+        Console.WriteLine(activationKey);
+    }
+}
+Console.WriteLine($"Your activation key is: {activationKey}");
---
+++ Preparation exercises on previous final exams/05. Programming Fundamentals Final Exam/02. Emoji Detector/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+string pattern = @"(\:\:|\*\*)([A-Z][a-z]{2,})\1";
+string text = Console.ReadLine();
+int coolThreshold = 1;
+
+foreach (char digit in text)
+{
+	if (char.IsDigit(digit))
+	{
+		int currentDigit = int.Parse(digit.ToString());
+		coolThreshold *= currentDigit;
+	}
+}
+
+MatchCollection emojis = Regex.Matches(text, pattern);
+Console.WriteLine($"Cool threshold: {coolThreshold}");
+Console.WriteLine($"{emojis.Count} emojis found in the text. The cool ones are:");
+
+foreach (Match match in emojis)
+{
+	int currentMatchValue = 0;
+    foreach (char item in match.Groups[2].Value)
+    {
+        currentMatchValue += item;
+    }
+
+    if (currentMatchValue >= coolThreshold)
+    {
+        Console.WriteLine(match.Value);
+    }
+}
