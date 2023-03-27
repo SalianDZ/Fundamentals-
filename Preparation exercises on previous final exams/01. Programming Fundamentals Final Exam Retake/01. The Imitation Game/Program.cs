5ba62e045402974c48f1f54fb3098005294d1129
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-27 23:13:27 +0300

    01. The Imitation Game
    Re-doing the exerises from the final exam

--- Preparation exercises on previous final exams/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game/Program.cs
+++ Preparation exercises on previous final exams/01. Programming Fundamentals Final Exam Retake/01. The Imitation Game/Program.cs
@@ -1,45 +1,41 @@
-﻿string decryptedMessage = Console.ReadLine();
-List<char> messageInChars = new List<char>();
-messageInChars.AddRange(decryptedMessage);
+﻿string encryptedMessage = Console.ReadLine();
 
 while (true)
 {
-    string[] command = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+    string[] command = Console.ReadLine()
+        .Split("|", StringSplitOptions.RemoveEmptyEntries);
 
-	if (command[0] == "Decode")
-	{
-		break;
-	}
-	else if (command[0] == "Move")
-	{
-        int lettersNumber = int.Parse(command[1]);
-		char[] temp = new char[lettersNumber];
-        for (int i = 0; i < lettersNumber; i++)
-		{
-			temp[i] = messageInChars[i];
-		}
-		messageInChars.RemoveRange(0, lettersNumber);
-		messageInChars.AddRange(temp);
-	}
-	else if (command[0] == "Insert")
-	{
-		int index = int.Parse(command[1]);
-		string value = command[2];
+    if (command[0] == "Decode")
+    {
+        break;
+    }
+    else if (command[0] == "Move")
+    {
+        int length = int.Parse(command[1]);
+        if (length > encryptedMessage.Length)
+        {
+            continue;
+        }
+        string substring = encryptedMessage.Substring(0, length);
+        encryptedMessage = encryptedMessage.Remove(0 , length);
+        encryptedMessage = encryptedMessage.Insert((encryptedMessage.Length), substring);
+    }
+    else if (command[0] == "Insert")
+    {
+        int index = int.Parse(command[1]);
+        if (index >= 0 && index <= encryptedMessage.Length)
+        {
+            string value = command[2];
+            encryptedMessage = encryptedMessage.Insert(index, value);
+        }
+    }
+    else if (command[0] == "ChangeAll")
+    {
+        string substring = command[1];
+        string replacement = command[2];
 
-		messageInChars.InsertRange(index, value);	
-	}
-	else if (command[0] == "ChangeAll")
-	{
-		char substring = char.Parse(command[1]);
-		char replacement = char.Parse(command[2]);
-
-        for (int i = 0; i < messageInChars.Count; i++)
-		{
-			if (messageInChars[i] == substring)
-			{
-				messageInChars[i] = replacement;
-			}
-		}
-	}
+        encryptedMessage = encryptedMessage.Replace(substring, replacement);
+    }
 }
-Console.WriteLine($"The decrypted message is: {string.Join("", messageInChars)}");
+
+Console.WriteLine($"The decrypted message is: {encryptedMessage}");
