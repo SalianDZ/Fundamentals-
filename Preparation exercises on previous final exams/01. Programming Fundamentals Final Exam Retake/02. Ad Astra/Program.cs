45636ac1ff6f37aed759596dd2e4e83cdc7f1e7c
Salian Dzhihanov <123269654+SalianDZ@users.noreply.github.com>
2023-03-28 00:07:58 +0300

    02. Ad Astra
    Re-doing exercises from the exam

--- Preparation exercises on previous final exams/01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
+++ Preparation exercises on previous final exams/01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
@@ -1,21 +1,19 @@
-﻿using System.Reflection.Metadata;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
+string pattern = @"(\#|\|)(?<foodName>[A-Za-z\s]+)\1(?<expirationDate>\d{2}\/\d{2}\/\d{2})\1(?<calories>\d+)\1";
 string text = Console.ReadLine();
-string pattern = @"(\||\#)(?<food>[A-Za-z\s]+)\1(?<date>(\d{2}\/)(\d{2}\/)(\d{2}))\1(?<calories>\d+)\1";
-int sum = 0;
+int caloriesSum = 0;
+MatchCollection foods = Regex.Matches(text, pattern);
 
-MatchCollection matchCollection = Regex.Matches(text, pattern);
-
-foreach (Match match in matchCollection)
+foreach (Match food in foods)
 {
-    int currentCalories = int.Parse(match.Groups["calories"].Value);
-    sum += currentCalories;
+    int currentCalories = int.Parse(food.Groups["calories"].Value);
+    caloriesSum += currentCalories;
 }
 
+Console.WriteLine($"You have food to last you for: {caloriesSum / 2000} days!");
 
-Console.WriteLine($"You have food to last you for: {sum / 2000} days!");
-foreach (Match match in matchCollection)
+foreach (Match food in foods)
 {
-    Console.WriteLine($"Item: {match.Groups["food"]}, Best before: {match.Groups["date"]}, Nutrition: {match.Groups["calories"]}");
+    Console.WriteLine($"Item: {food.Groups["foodName"].Value}, Best before: {food.Groups["expirationDate"].Value}, Nutrition: {food.Groups["calories"].Value}");
 }
